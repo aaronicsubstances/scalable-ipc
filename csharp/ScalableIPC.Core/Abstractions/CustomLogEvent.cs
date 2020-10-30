591125f22f2c6191840ae642ae3d466b5403d213
Aaron Baffour-Awuah <aaronbaffourawuah@gmail.com>
2020-10-30 11:44:39 +0000

    Added abstractions needed for log-based testing.

---
+++ csharp/ScalableIPC.Core/Abstractions/CustomLogEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScalableIPC.Core.Abstractions
+{
+    public class CustomLogEvent
+    {
+        public string Id { get; set; }
+        public string Message { get; set; }
+        public IDictionary<string, object> Data { get; set; }
+    }
+}
---
+++ csharp/ScalableIPC.Core/Abstractions/ICustomLogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScalableIPC.Core.Abstractions
+{
+    public interface ICustomLogger
+    {
+        bool Enabled { get; }
+        void Log(CustomLogEvent logEvent);
+    }
+}
