c608941ca6b9dabd3b4cab491710e21b4e447c58
Aaron Baffour-Awuah <aaronbaffourawuah@gmail.com>
2020-11-15 18:21:53 +0000

    Restored comment on possible network transport implementations.

--- csharp/ScalableIPC.Core/Abstractions/INetworkTransportInterface.cs
+++ csharp/ScalableIPC.Core/Abstractions/INetworkTransportInterface.cs
@@ -2,6 +2,21 @@
 
 namespace ScalableIPC.Core.Abstractions
 {
+    /// <summary>
+    /// Abstracting underlying network allows us to separately target different networks such as
+    /// 1. fake socket for testing
+    /// 2. datagram socket on localhost.
+    /// 3. TCP/TLS, implemented as connection pooling with either fixed size or maximum size.
+    /// A key feature is automatic retry in some cases: when an error occurs on an existing connection,
+    /// an attempt is immediately made to create a new one to replace it. This feature is the key to
+    /// alleviating programmers from the pains of using custom protocols over TCP.
+    ///    2 variants of this are:
+    ///    A. this retry attempt is made for all pdus. Intended for maximum size connection pooling
+    ///       with no load balancing involved.
+    ///    B. this retry attempt is made only for window id=0 pdus. Intended for fixed size connection pooling in which
+    ///       load balancing is involved.
+    /// 4. datagram socket and DTLS on the Internet. Futuristic and intended for gamers and others to implement.
+    /// </summary>
     public interface INetworkTransportInterface
     {
         AbstractPromiseApi PromiseApi { get; set; }
